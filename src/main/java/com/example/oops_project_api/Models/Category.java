7fffb004e89b5ecb65bd255928c68cc95b2417c0
charanbhatia <charanbhatia05@gmail.com>
2024-04-24 03:50:15 +0530

    Modified pom.xml | created database and connected category and product with it

--- src/main/java/com/example/oops_project_api/Models/Category.java
+++ src/main/java/com/example/oops_project_api/Models/Category.java
@@ -1,11 +1,15 @@
 package com.example.oops_project_api.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Category {
+    @jakarta.persistence.Id
     private Long Id;
     private String title;
     private  String description;
--- src/main/java/com/example/oops_project_api/Models/Product.java
+++ src/main/java/com/example/oops_project_api/Models/Product.java
@@ -1,17 +1,23 @@
 package com.example.oops_project_api.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 //@AllArgsConstructor
 public class Product {
+    @jakarta.persistence.Id
     private Long Id;
     private String title;
     private  String description;
     private Double price;
+    @ManyToOne
     private Category category;
     private  String image;
 
